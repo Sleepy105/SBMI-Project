3da86468429c600b6afcd39a1137cb7091b03b14
Sleepy105 <lm.sousa@fe.up.pt>
2018-10-29 15:24:33 +0000

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ *  main.c
+ *
+ *  Created on: Oct 29, 2018
+ *      Author: Luís Sousa, Leonor Santos
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main() {
+  return 0;
+}

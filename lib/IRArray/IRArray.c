2ee741c1c4bc97efdfb49448c1686e65292cb8d1
Leonor Santos <ronoelsantos@gmail.com>
2018-12-12 13:32:36 +0000

    init ir

--- lib/IRArray/IRArray.c
+++ lib/IRArray/IRArray.c
@@ -1,7 +1,8 @@
 #include <IRArray.h>
 
 void initArrayHardware() {
-
+    DDRC~&= ~(1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);
+    PORTC |= (1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);
 }
 
 uint8_t updateSensorValuesArray() {

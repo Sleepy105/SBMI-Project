641693c7ba07c160d8d7c3d093169fa11662418b
Leonor Santos <ronoelsantos@gmail.com>
2018-12-12 13:53:10 +0000

    ir update

--- lib/IRArray/IRArray.c
+++ lib/IRArray/IRArray.c
@@ -1,10 +1,13 @@
 #include <IRArray.h>
 
 void initArrayHardware() {
-    DDRC~&= ~(1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);
-    PORTC |= (1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);
+    DDRC~&= ~(1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);     /* IR sensor pins as inputs*/
+    PORTC |= (1<<IR_0 | 1<<IR_1 | 1<<IR_2 | 1<<IR_3 | 1<<IR_4);         /* pull-up resistor on*/
 }
 
 uint8_t updateSensorValuesArray() {
-    return 0x00;
+    
+    IR_vector=  PINB &  0<1F;
+
+    return IR_vector;
 }

014b222b91b672d372b566366edd91729f9831ff
Leonor Santos <ronoelsantos@gmail.com>
2018-12-28 10:43:54 +0000

    descomentar initExternalInterrupts()

--- src/main.c
+++ src/main.c
@@ -220,7 +220,7 @@ void initEEPROM() {
 void initHardware() {
     cli(); // Disable all interrupts during hardware configuration
 
-    //initExternalInterrupts();
+    initExternalInterrupts();
     initMotors();
     initWatchdogTimer();
     setReceiveCallback(&handleReceivedByte);
